fedf434574d93f630af51384e604d3af8760c7c2
hardel99 <harold-delgado1999@hotmail.com>
2019-01-11 16:32:40 -0600

    First Commit yeah

---
+++ app/src/main/java/com/interfaz/hardel/sivarportal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.interfaz.hardel.sivarportal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
